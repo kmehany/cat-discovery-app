94f9646a7980a82d377013e2b5beb3a2d65fd160
Kerolos Mehany <kerolosmehany@Keroloss-Air.eduroam.gmu.edu>
2026-04-21 22:12:53 -0400

    added a homepage with background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import { redirect } from "next/navigation";
+
+import Link from "next/link";
+import "./discover.css";
+
 
 export default function Home() {
-  redirect("/discover");
+  return (
+    <div className="page">
+      <div className="home-card">
+        <h1 className="title">Welcome to the Cat Explore Planet</h1>
+
+        <p className="subtitle">
+          Explore random cats, view their attributes, and ban traits you do not want to see again.
+        </p>
+
+        <div className="home-button-group">
+          <Link href="/discover" className="home-btn">
+            Go to Discover Page
+          </Link>
+
+
+        </div>
+      </div>
+    </div>
+  );
 }
